b567d4d23422b64a209e162fc1c175c337714b7e
Kyankya Raymond <rkyankya@rkyankya.rkyankya>
2020-01-23 19:03:04 +0300

    Added User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
   def new
   # Write new here
+    @user = User.new
   end
 
   def create
-  # Write create here
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to new_user_path
+    else
+      render "new"
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
