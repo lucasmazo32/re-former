4616bb7dab6e61c52e15901f4cc85222f32846c5
rkyankya@gmail.com <rkyankya@rkyankya.rkyankya>
2020-01-25 15:53:41 +0300

    :ok_hand: Added plain html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to users_path
     else
       render 'new'
     end
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     @user.update(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to users_path
     else
       render 'edit'
     end
