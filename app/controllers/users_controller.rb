772e8bc356bb20a4d8b8a191687e8781eefc40bc
rkyankya@gmail.com <rkyankya@rkyankya.rkyankya>
2020-01-25 15:59:03 +0300

    :shirt: Fixed Sticker ci errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,9 @@ class UsersController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
+  
 end
